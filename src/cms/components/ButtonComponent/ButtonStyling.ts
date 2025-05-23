50bf00bbc8dcff84eb0e8ca88db6e378dae19dfd
Dan Isaacs <daniel.isaacs@optimizely.com>
2025-05-23 17:15:43 -0600

    Reduce code duplication; add button styles

--- src/cms/components/ButtonComponent/ButtonStyling.ts
+++ src/cms/components/ButtonComponent/ButtonStyling.ts
@@ -1,7 +1,7 @@
 import type { DisplaySettingsFragment } from '../../../../__generated/sdk.ts';
 import { getDictionaryFromDisplaySettings } from '../../../graphql/shared/displaySettingsHelpers.ts';
 
-export function getButtonStyles(displaySettings: DisplaySettingsFragment[]): {
+export function getButtonStyles(displaySettings: DisplaySettingsFragment[], needButtonType: Boolean = true): {
     // buttonStyle: string;
     // buttonType: string;
 } {
@@ -9,11 +9,13 @@ export function getButtonStyles(displaySettings: DisplaySettingsFragment[]): {
         getDictionaryFromDisplaySettings(displaySettings);
     
     let cssClasses: string[] = [];
-
+    
+    // Button text transform
     if(settings['transform'] && settings['transform'] !== "keep") {
         cssClasses.push(settings['transform']);
     }
 
+    // Button style
     const buttonStyle = settings['buttonStyle'];
     const allButtonStyles : { [key: string]: any } = 
         {standard: 'btn-standard',
@@ -29,25 +31,30 @@ export function getButtonStyles(displaySettings: DisplaySettingsFragment[]): {
     if(buttonStyle) {
         cssClasses.push(`${allButtonStyles[buttonStyle]}`);
     }
-
-    const buttonType = settings['buttonType'];
-    const allButtonTypes  : { [key: string]: any } = 
-        {primary: 'btn-primary',
-         secondary: 'btn-secondary',
-         neutral: 'btn-neutral',
-         accent: 'btn-accent',
-         info: 'btn-info',
-         success: 'btn-success',
-         warning: 'btn-warning',
-         error: 'btn-error'
-        };
-    if(buttonType) {
-        cssClasses.push(`${allButtonTypes[buttonType]}`);
-    } else {
-        // Default type: btn-primary
-        cssClasses.push('btn-primary');
+    
+    // Button type
+    // Note: button type added to CallToAction and Card buttons directly
+    if (needButtonType) { 
+        const buttonType = settings['buttonType'];
+        const allButtonTypes  : { [key: string]: any } = 
+            {primary: 'btn-primary',
+            secondary: 'btn-secondary',
+            neutral: 'btn-neutral',
+            accent: 'btn-accent',
+            info: 'btn-info',
+            success: 'btn-success',
+            warning: 'btn-warning',
+            error: 'btn-error'
+            };
+        if(buttonType) {
+            cssClasses.push(`${allButtonTypes[buttonType]}`);
+        } else {
+            // Default type: btn-primary
+            cssClasses.push('btn-primary');
+        }
     }
 
+    // Button width
     const buttonWidth = settings['buttonWidth'] ?? '';
     let widthClass = 'w-[9rem]';
     switch (buttonWidth) {
@@ -87,6 +94,7 @@ export function getButtonStyles(displaySettings: DisplaySettingsFragment[]): {
     }
     cssClasses.push(widthClass);
 
+    // Button size
     const buttonSize = settings['buttonSize'] ?? '';
     let sizeClass = '';
     switch (buttonSize) {
@@ -113,6 +121,34 @@ export function getButtonStyles(displaySettings: DisplaySettingsFragment[]): {
             break;
     }
     cssClasses.push(sizeClass);
+    
+    // Button rounded corners
+    const buttonRadius = settings['buttonRadius'];
+    const allButtonRadii : { [key: string]: any } = 
+        {standard: 'rounded-none',
+         xs: 'rounded-xs',
+         sm: 'rounded-sm',
+         md: 'rounded-md',
+         lg: 'rounded-lg',
+         xl: 'rounded-xl',
+         xl2: 'rounded-2xl',
+         xl3: 'rounded-3xl',
+         xl4: 'rounded-4xl',
+         full: 'rounded-full'
+        };
+    if(buttonRadius) {
+        cssClasses.push(`${allButtonRadii[buttonRadius]}`);
+    }
 
+    // Button rounded corners
+    const buttonAction = settings['buttonAction'];
+    const allButtonAction : { [key: string]: any } = 
+        {static: '',
+         bouncy: 'transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg'
+        };
+    if(buttonAction) {
+        cssClasses.push(`${allButtonAction[buttonAction]}`);
+    }
+    
     return cssClasses;
 }
