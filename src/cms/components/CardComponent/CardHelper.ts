8bc7eafe44dd5fff667ad43ff89f917ecd9f3e05
Dan Isaacs <daniel.isaacs@optimizely.com>
2026-06-12 00:01:11 -0600

    Fix card display split styles

--- src/cms/components/CardComponent/CardHelper.ts
+++ src/cms/components/CardComponent/CardHelper.ts
@@ -11,6 +11,19 @@ export function getAssetWidthClass(dictionary: Record<string, string>): string {
     }
 }
 
+export function getMdAssetWidthClass(dictionary: Record<string, string>): string {
+    const widthValue = dictionary['assetWidth'];
+    switch (widthValue) {
+        case 'flex_1': return 'md:flex-1';
+        case 'w_1_3': return 'md:w-1/3';
+        case 'w_2_5': return 'md:w-2/5';
+        case 'w_1_2': return 'md:w-1/2';
+        case 'w_3_5': return 'md:w-3/5';
+        case 'w_2_3': return 'md:w-2/3';
+        default: return 'md:w-1/2';
+    }
+}
+
 export function getContentWidthClass(dictionary: Record<string, string>): string {
     const widthValue = dictionary['contentWidth'];
     switch (widthValue) {
@@ -21,5 +34,18 @@ export function getContentWidthClass(dictionary: Record<string, string>): string
         case 'w_3_5': return 'w-3/5';
         case 'w_2_3': return 'w-2/3';
         default: return 'flex-1';
+    }
+}
+
+export function getMdContentWidthClass(dictionary: Record<string, string>): string {
+    const widthValue = dictionary['contentWidth'];
+    switch (widthValue) {
+        case 'flex_1': return 'md:flex-1';
+        case 'w_1_3': return 'md:w-1/3';
+        case 'w_2_5': return 'md:w-2/5';
+        case 'w_1_2': return 'md:w-1/2';
+        case 'w_3_5': return 'md:w-3/5';
+        case 'w_2_3': return 'md:w-2/3';
+        default: return 'md:w-1/2';
     }
 }
